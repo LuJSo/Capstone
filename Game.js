0ead46ae4b133a318d3261f10830ee158e1cb2f6
Lucas S <sushiguy132@gmail.com>
2017-05-11 09:18:30 -0700

    sync

--- Game.js
+++ Game.js
@@ -1,21 +1,19 @@
+/***********/
+/* Used video blah blah blah */
+/************/
 
-var text = document.getElementById("text")
+
+ var text = document.getElementById("text")
  var buttonBox = document.getElementById('submitButton')
  var input = document.getElementById('input')
  var player
  var score = 0
- var scoreText = makeText("Score: -", 660, 50, 25, "sans-serif", "white", 1)
+ var scoreWords = makeText("Score: ", 660, 50, 25, "sans-serif", "white", 1)
+ var scoreText = makeText(" -", 720, 50, 25, "sans-serif", "white", 1)
   var namespace = "http://www.w3.org/2000/svg"
 
 
- input.onkeypress = function(event) {
-   console.log(input.value);
-   if(event.key == "Enter" || event.keyCode == 13) {
-     player = input.value;
-     input.parentNode.removeChild(input)
-     advanceTo(scenario.two)
-   }
- }
+
  function makeText(message, x, y, fontSize, fontFamily, fill, opacity) {
    var text = document.createElementNS(namespace, "text")
    text.innerHTML = message
@@ -47,20 +45,28 @@ var buttonChange = function(ListButtons) {
 
 
 
-var advanceTo = function(s) {
+function advanceTo(s) {
 
   changeText(s.text)
   buttonChange(s.buttons)
 
+ };
+function addScore(s) {
+  score = score +1
+  scoreText.innerHTML = score
+  advanceTo(s)
+}
 
-};
-
+function passState() {
+  
+}
 
 
 var scenario = {
   one: {
 
-    text: "You are walking and you see an old house, who wouldn't you want to go inside?\n",
+    text: "You are walking and you see an old house, who wouldn't you want to go inside?",
+    buttons:  [["Enter House", "advanceTo(scenario.two)"]]
   },
   two: {
 
@@ -74,18 +80,22 @@ var scenario = {
   },
     four: {
     text: "you proceed down the corridor, you arrive in a dimly lit room with a note on the wall and a small door next to it.  ",
-    buttons: [["Collect note",  "advanceTo(scenario.five)"],["don't collect note", "advanceTo(scenario.random)"]]
+    buttons: [["Collect note",  "addScore(scenario.five)"],["don't collect note", "advanceTo(scenario.random)"]]
 },
 
 random: {
   text: "you did not collect the note and you attempt to leave, but all the exits seem to have disappeared. ",
-  buttons: [["Collect note", "advanceTo(scenario.five)"]]
+  buttons: [["Collect note", "addScore(scenario.five)"]]
 },
 
     five: {
     text: "After collecting the note which appears to have chicken scratch written on it, the small door opens with a dimly lit corridor behind it.",
      buttons: [["Walk through corridor", "advanceTo(scenario.six)"]]
   },
+
+  six: {
+    text: "Walking through the corridor, you hear footsteps and something breathing moving rapidly towards you"
+  }
 
 };
 advanceTo(scenario.one);
