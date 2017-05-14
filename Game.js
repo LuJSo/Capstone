024da60c8374f0d83b7e1d0ea165f16ec0caab03
Lucas S <sushiguy132@gmail.com>
2017-05-14 00:09:46 -0700

    finished

--- Game.js
+++ Game.js
@@ -1,5 +1,5 @@
 /***********/
-/* Used video blah blah blah */
+/* used code from codepen user http://codepen.io/rzim2082/
 /************/
 
 
@@ -11,6 +11,7 @@
  var scoreWords = makeText("Score: ", 660, 50, 25, "sans-serif", "white", 1)
  var scoreText = makeText(" -", 720, 50, 25, "sans-serif", "white", 1)
   var namespace = "http://www.w3.org/2000/svg"
+
 
 
 
@@ -55,11 +56,17 @@ function addScore(s) {
   score = score +1
   scoreText.innerHTML = score
   advanceTo(s)
-}
+ }
 
-function passState() {
-  
-}
+ function gameOver() {
+location.reload()
+ }
+
+ function addThree(s) {
+   score = score +3
+   scoreText.innerHTML = score
+   advanceTo(s)
+  }
 
 
 var scenario = {
@@ -88,14 +95,42 @@ random: {
   buttons: [["Collect note", "addScore(scenario.five)"]]
 },
 
+death: {
+  text: "It has no face, tentacles on it's back and it's heading right towards you",
+  buttons: [["Game over, click to restart", "gameOver()"]]
+},
+
+death2: {
+  text: "The room turns pitch black, you hear the breathing right behind you",
+  buttons: [["Game over, click to restart", "gameOver()"]]
+},
+
     five: {
     text: "After collecting the note which appears to have chicken scratch written on it, the small door opens with a dimly lit corridor behind it.",
      buttons: [["Walk through corridor", "advanceTo(scenario.six)"]]
   },
 
   six: {
-    text: "Walking through the corridor, you hear footsteps and something breathing moving rapidly towards you"
-  }
+  text: "Walking through the corridor, you hear footsteps and something breathing moving rapidly towards you",
+   buttons: [["Turn around", "advanceTo(scenario.death)"], ["Continue walking", "advanceTo(scenario.seven)"]]
+},
+
+ seven: {
+   text: "Sweat is coming down the side of your face, the breathing is getting louder but you reach the end of the hall.  You enter a door to find a large room and at the end of the room there is a skeleton wearing some sort of mailman outfit, next to it is a small bag",
+   buttons: [["Check inside the bag", "advanceTo(scenario.eight)"], ["Leave the bag alone", "advanceTo(scenario.death2)"]]
+ },
+
+ eight: {
+   text: "Inside the bag you find three notes",
+   buttons: [["Collect notes and move to next room", "addThree(scenario.nine)"]]
+ },
+
+ nine: {
+   text: "You move into the next room thinking if you collect all the pages you can figure out how to escape this place but in the next room you see it",
+   buttons: [["Collect notes and move to next room", "addThree(scenario.death)"]]
+ },
+
+
 
 };
 advanceTo(scenario.one);
